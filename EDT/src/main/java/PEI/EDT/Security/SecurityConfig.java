cc2a4dc9e1bd6f4c044d2462e77582cd5befb5de
Mohamed Lemine <24013@esp.mr>
2026-01-11 13:21:54 +0000

    Adding Swagger UI

--- EDT/src/main/java/PEI/EDT/Security/SecurityConfig.java
+++ EDT/src/main/java/PEI/EDT/Security/SecurityConfig.java
@@ -22,9 +22,19 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        // Auth
                         .requestMatchers("/api/auth/**").permitAll()
+
+                        // Swagger / OpenAPI
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+
                         .anyRequest().authenticated()
                 )
+
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
